a3ffea2f5617f736d553a4353ba5fdfc6fef184f
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-01 17:40:48 +0200

    add factories for creating game objects

---
+++ script.js
@@ -0,0 +1,40 @@
+const createPlayer = function(name, marker) {
+    let score = 0;
+    const getScore = () => score;
+    const setScore = () => { score++; };
+    return {name, marker, getScore, setScore};
+};
+
+const Gameboard = (() => {
+    const board = ["","","","","","","","",""];
+    const getBoard = () => board;
+    return {getBoard };
+})();
+
+const winChecker = function(Gameboard) {
+    winConditions = [[0,1,2],[3,4,5],[6,7,8],
+                     [0,3,6],[1,4,7],[2,5,8],
+                     [0,4,8],[2,4,6]
+                    ];
+    const isDraw = () => {
+        return !Gameboard.includes("") && !hasWinner();
+    }
+
+    const hasWinner = () => {
+        for (const win of winConditions) {
+            if (Gameboard[win[0]] === "" ||
+                Gameboard[win[1]] === "" ||
+                Gameboard[win[2]] === ""
+            ) continue;
+
+            if (Gameboard[win[0]] === Gameboard[win[1]] &&
+                Gameboard[win[0]] === Gameboard[win[2]]
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return {isDraw, hasWinner };
+}
